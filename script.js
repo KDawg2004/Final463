9ca056cf6a341073a566404c061e4004bb38ad07
Kaevin barta <kbarta@pdx.edu>
2026-06-07 11:46:59 -0700

    late push, about page done, projects page done, still need to polish and add features

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('shadow-sm');
   }
 });
+
+// ---- WORK ----
+const workCards = document.querySelectorAll('#work-cards .card');
+
+for (const card of workCards) {
+  card.addEventListener('click', () => {
+    const details = card.querySelector('.card-details');
+    const hint = card.querySelector('.click-hint');
+    if (details.style.display === 'none') {
+      details.style.display = 'block';
+      hint.style.display = 'none';
+    } else {
+      details.style.display = 'none';
+      hint.style.display = 'block';
+    }
+  });
+}
+
